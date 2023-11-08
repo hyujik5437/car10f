a6bb4ca13a342320cc5e34a8367aa02164b92803
Dima Tudor-Daniel <106043858+hyujik5437@users.noreply.github.com>
2023-11-08 09:58:13 +0200

    Add files via upload

---
+++ car 10f/Car.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_10f
+{
+    public class Car
+    {
+        public string color;
+        public string name;
+        public int maxSpeed;
+        public int year;
+
+        public Car(string nameName, string colorName, int yearValue, int maxSpeedValue)
+        {
+            name = nameName;
+            color= colorName;
+            year = yearValue;
+            maxSpeed = maxSpeedValue;
+        }
+
+        public Car(string nameName) 
+        {
+            name = nameName;
+        }
+        public void fullThrottle()
+        {
+            Console.WriteLine(name + " is going a the speed of " + maxSpeed);
+        }
+    }
+}
---
+++ car 10f/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace car_10f
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car Ford = new Car("Mustang","white", 2021, 100);
+            Car Opel = new Car("Astra", "blue", 2020, 120);
+            Car Volvo = new Car("XC-90");
+          //  Ford.name = "Mustang";
+           // Ford.year = 2021;
+           // Ford.color = "white";
+           // Ford.maxSpeed = 100;
+           // Ford.fullThrottle();
+          //  Opel.name = "Astra";
+          //  Opel.year = 2020;
+           // Opel.color = "blue";
+          //  Opel.maxSpeed = 120;
+          //  Opel.fullThrottle();
+
+
+            Console.WriteLine(Ford.name);
+            Console.WriteLine(Ford.color);
+            Console.WriteLine(Ford.year);
+            Ford.fullThrottle();
+            Console.WriteLine(Opel.name);
+            Console.WriteLine(Opel.color);
+            Console.WriteLine(Opel.year);
+            Opel.fullThrottle();
+            Console.WriteLine(Volvo.name);
+         Console.ReadKey();
+        }
+    }
+}
